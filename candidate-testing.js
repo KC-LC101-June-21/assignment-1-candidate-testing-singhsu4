d37c18d8c7e0db5136b45666767bf22102f092bf
singhsu4 <singhsu4@gmail.com>
2021-06-09 20:52:54 +0000

    This is my first part of assignment 1 submission!

--- candidate-testing.js
+++ candidate-testing.js
@@ -6,28 +6,30 @@ const input = require('readline-sync');
 let candidateName;
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question;
-let correctAnswer;
+let correctAnswer= ("Sally Ride");
 let candidateAnswer;
 let questions;
 let correctAnswers;
 let candidateAnswers;
 
 
+
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+candidateName = input.question("Please enter your name: ");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+question= input.question("Who was the first American woman in Space? ");
 
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
+console.log("Correct Answer:  " + correctAnswer + "");
+console.log("Candidate Answer: " + question + "");
 
   let grade;
   
@@ -38,7 +40,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  
+  console.log("Welcome to the Fury Road, " +candidateName+ "!");
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
