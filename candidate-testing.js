9107c677bf3d8e793ddd1143c58e5e8b338fb113
singhsu4 <singhsu4@gmail.com>
2021-06-18 19:46:13 +0000

    Changed female to woman in line 8

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,7 +5,7 @@
 // TODO 1.1a: Define candidateName // 
 let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = "Who was the first American female in Space? ";
+let question = "Who was the first American woman in Space? ";
 let correctAnswer = ("Sally Ride");
 let candidateAnswer = "";
 let questions = [
